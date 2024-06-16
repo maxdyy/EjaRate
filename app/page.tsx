3e348a98a2f2880278055ed5bc7e7786e139de63
Maksym Dmukhovskyy <dm.maksym@gmail.com>
2024-06-16 13:07:48 +0400

    added search title to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center pt-32 lg:pt-64 px-4">
         <span className="text-xl lg:text-2xl font-bold">EjaRate</span>
         <h1 className="text pb-5 lg:text-xl text-center">
-          Search for <span className="font-semibold">Reviews</span> in your{" "}
+          Search for <span className="font-semibold">Rent Reviews</span> in your{" "}
           <span className="font-semibold">Building</span>
         </h1>
         <SearchBar />
