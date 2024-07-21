ed2a3c75fdfd52830656005b71c0bbed85a24629
Maksym Dmukhovskyy <dm.maksym@gmail.com>
2024-07-21 17:22:32 +0400

    Review page with data from the DB

---
+++ app/api/review/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest } from "next/server";
+import { supabase } from "@/lib/supabase";
+
+export async function GET(req: NextRequest) {
+  // Get the id query parameter from the URL
+  const searchID = req.nextUrl.searchParams.get("id");
+
+  // Now that we have the ID, we can query the database for the review data
+  if (searchID) {
+    const { data, error } = await supabase
+      .from("reviews")
+      .select("*")
+      .eq("id", searchID)
+      .eq("is_approved", true);
+
+    if (error) {
+      console.error(error);
+      return Response.json({
+        reviewData: null,
+        error: "Error fetching building data",
+      });
+    } else {
+      return Response.json({ reviewData: data });
+    }
+  }
+}
--- app/building/[id]/page.tsx
+++ app/building/[id]/page.tsx
@@ -3,21 +3,45 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/Card";
+import { Card, CardContent } from "@/components/ui/Card";
+import { Button } from "@/components/ui/Button";
+import { Separator } from "@/components/ui/Separator";
 import { BuildingReviewsData } from "@/lib/interfaces";
 import { getBuildingData } from "@/lib/services";
 import { formatBuildingName } from "@/lib/utils";
 
 export default function BuildingPage({ params }: { params: { id: string } }) {
   const [searching, setSearching] = useState<boolean>(true);
-  const [buildingData, setBuildingData] = useState<BuildingReviewsData | null>(
-    null
-  );
+  const [buildingData, setBuildingData] = useState<
+    BuildingReviewsData[] | null
+  >(null);
+  const [buildingName, setBuildingName] = useState<string | null>(null);
+  const [buildingAddress, setBuildingAddress] = useState<string | null>(null);
+  const [totalReviews, setTotalReviews] = useState<number | null>(null);
+  const [averageReview, setAverageReview] = useState<number | null>(null);
+  const [apartmentAverage, setApartmentAverage] = useState<number | null>(null);
 
   useEffect(() => {
     const fetchBuildingData = async () => {
+      // Here I am setting all the reviews into our state
       const data = await getBuildingData(params.id);
-      setBuildingData(data);
+      if (data && data.length > 0) {
+        setBuildingData(data);
+        setTotalReviews(data.length);
+        // We will use the first review to get the building name and address
+        setBuildingName(data[0].building_name);
+        setBuildingAddress(data[0].building_address);
+        // Calculate the average reviews
+        let total = 0;
+        let apartmentTotal = 0;
+        data.forEach((review: BuildingReviewsData) => {
+          total += review.building_quality || 0;
+          apartmentTotal += review.apartment_quality || 0;
+        });
+
+        setAverageReview(total / data.length);
+        setApartmentAverage(apartmentTotal / data.length);
+      }
       setSearching(false);
     };
 
@@ -25,7 +49,7 @@ export default function BuildingPage({ params }: { params: { id: string } }) {
   }, [params.id]);
 
   return (
-    <main className="h-[calc(100vh-128px)] lg:h-[calc(100vh-110px)] flex justify-center">
+    <main className="flex justify-center">
       {searching && (
         <div className="pt-40 text-center">
           <p>Searching for reviews...</p>
@@ -41,18 +65,68 @@ export default function BuildingPage({ params }: { params: { id: string } }) {
             </span>
           </p>
           <p className="pt-4">
-            <Link className="underline" href="/">
-              Back to the Home Page
+            <Link className="underline" href="/submit-review">
+              Write a review
             </Link>
           </p>
         </div>
       )}
 
       {buildingData && !searching && (
-        <div className="pt-40 text-center">
-          <h1 className="text-2xl font-semibold"></h1>
-          <p className="pt-5 text-lg font-semibold">Average Review: </p>
-          <div className="pt-5 flex"></div>
+        <div className="pt-40 text-center w-full max-w-[850px]">
+          <h1 className="text-2xl font-semibold">{buildingName}</h1>
+          <p className="pt-2 text-lg">{buildingAddress}</p>
+          <div className="flex w-full justify-between">
+            <p className="pt-5 text-lg font-semibold">
+              Total Reviews: {totalReviews}
+            </p>
+            <p className="pt-5 text-lg font-semibold">
+              Average Review: {averageReview}
+            </p>
+            <p className="pt-5 text-lg font-semibold">
+              Average Apartment Review: {apartmentAverage}
+            </p>
+          </div>
+          <div className="pt-12 flex">
+            {buildingData.map((review: BuildingReviewsData) => (
+              <div key={review.id} className="w-1/3 p-2">
+                <Card>
+                  <CardContent className="text-left pt-4">
+                    <p>
+                      <span className="font-semibold">Building Quality:</span>{" "}
+                      {review.building_quality}
+                    </p>
+                    <p>
+                      <span className="font-semibold">Apartment Quality:</span>{" "}
+                      {review.apartment_quality}
+                    </p>
+                    <div className="py-2">
+                      <Separator />
+                    </div>
+                    <div>
+                      {review.rent_amount && (
+                        <p>Rent Amount: {review.rent_amount}</p>
+                      )}
+                      {review.agency_name && (
+                        <p>Agency Name: {review.agency_name}</p>
+                      )}
+                      {review.agency_experience && (
+                        <p>Agency Experience: {review.agency_experience}</p>
+                      )}
+                      <div className="pt-2">
+                        <Separator />
+                      </div>
+                      <div className="pt-4 text-center">
+                        <Button size="sm" className="w-full">
+                          <Link href={`/review/${review.id}`}>View Review</Link>
+                        </Button>
+                      </div>
+                    </div>
+                  </CardContent>
+                </Card>
+              </div>
+            ))}
+          </div>
         </div>
       )}
     </main>
