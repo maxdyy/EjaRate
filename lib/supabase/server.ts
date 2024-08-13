779d15a9eddf6c4f1c64da9d357bfa755401dfad
Maksym Dmukhovskyy <dm.maksym@gmail.com>
2024-08-13 12:24:22 +0400

    fix: fixed cookies server log error

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,6 +1,6 @@
 // Modified setup from https://supabase.com/docs/guides/auth/server-side/nextjs
 
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export const createClientServer = () => {
@@ -11,21 +11,18 @@ export const createClientServer = () => {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value;
+        getAll() {
+          return cookieStore.getAll();
         },
-        set(name: string, value: string, options: CookieOptions) {
+        setAll(cookiesToSet) {
           try {
-            cookieStore.set({ name, value, ...options });
-          } catch (error) {
-            console.error(error);
-          }
-        },
-        remove(name: string, options: CookieOptions) {
-          try {
-            cookieStore.delete({ name, ...options });
-          } catch (error) {
-            console.error(error);
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            );
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
           }
         },
       },
