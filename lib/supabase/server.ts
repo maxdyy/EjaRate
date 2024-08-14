c707e2b0c62a84744150bf2e90e683efa0b653c9
Maksym Dmukhovskyy <dm.maksym@gmail.com>
2024-08-14 12:32:56 +0400

    fix: fixing cookies settings and user info access on client

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -23,7 +23,7 @@ export const createClientServer = () => {
             // The `setAll` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
             // user sessions.
-            console.error("Error from setAll cookies Supabase:", e);
+            console.error("Error from setAll cookies Supabase server:", e);
           }
         },
       },
